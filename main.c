243ab1bd967ee1ac2dff61d7105e8182d7d86a8d
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-12-12 17:58:22 +0000

    still trying

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned int layers[5] = {38, 31, 24, 11, 9};
-	struct fann *ann = fann_create_standard_array(5, layers);
-	fann_set_activation_function_hidden(ann, FANN_LINEAR);
+	unsigned int layers[3] = {38, 17, 9};
+	struct fann *ann = fann_create_standard_array(3, layers);
+	fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
     	fann_set_activation_function_output(ann, FANN_LINEAR);
-	fann_train_on_file(ann, "scotland.data", 100000, 1000, 1);
+	fann_train_on_file(ann, "scotland.data", 100000, 100, 0.00001);
 	fann_save(ann, "scotland_test.net");
 	fann_destroy(ann);
 	return 0;
